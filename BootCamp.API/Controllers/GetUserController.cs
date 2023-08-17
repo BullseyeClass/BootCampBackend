45f55dd1140d9a34eee2bbeddffea47367e726ac
Adigun Pelumi <Adigun.P@Bullseyemanpower.com>
2023-08-17 12:18:32 +0100

    Backend

---
+++ BootCamp.API/Controllers/GetUserController.cs
@@ -0,0 +1,43 @@
+﻿using BootCamp.Data.Context;
+using BootCamp.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BootCamp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetUserController : ControllerBase
+    {
+
+        private readonly MyAppContext _appContext;
+
+        public GetUserController(MyAppContext myAppContext)
+        {
+            _appContext = myAppContext;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Trainee>> GetUserById(string id)
+        {
+            //if (string.IsNullOrEmpty(id))
+            //{
+            //    return BadRequest("Invalid ID");
+            //}
+
+            var trainee = await _appContext.Users.FindAsync(id);
+
+            if (trainee == null)
+            {
+                return NotFound();
+            }
+
+            return trainee; 
+        }
+
+
+    }
+}
