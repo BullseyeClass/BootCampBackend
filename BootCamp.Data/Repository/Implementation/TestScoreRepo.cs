1b6e063567e8dfdaee59020f50eb33145584511e
Olawole Temitope <Olawole.T@Bullseyemanpower.com>
2023-08-17 15:02:42 +0100

    TestScore Repo and Interface Implemented

---
+++ BootCamp.Data/Repository/Implementation/TestScoreRepo.cs
@@ -0,0 +1,29 @@
+﻿using BootCamp.Data.Context;
+using BootCamp.Data.Entities;
+using BootCamp.Data.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCamp.Data.Repository.Implementation
+{
+
+    public class TestScoreRepository : ITestScoreRepository
+    {
+        private readonly MyAppContext _dbContext;
+
+        public TestScoreRepository(MyAppContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Test> GetTestScoresByUserId(string userId)
+        {
+            return _dbContext.Tests
+                .Where(ts => ts.StudentId == userId)
+                .ToList();
+        }
+    }
+}
---
+++ BootCamp.Data/Repository/Interface/ITestScoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCamp.Data.Repository.Interface
+{
+    internal interface ITestScore
+    {
+    }
+}
